65737a7bf60f1d6e0c72f5cc97cdbddbe2b26832
Jean-Michele <morin.emilemorin@gmail.com>
2025-02-22 13:15:44 -0500

    Terminer le projet
    a

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
 using UnityEngine.InputSystem;
+using UnityEngine;
 using UnityEngine.Windows;
 
 public class PlayerInput : MonoBehaviour
@@ -10,6 +11,7 @@ public class PlayerInput : MonoBehaviour
     private Rigidbody _sub;
     private Vector3 directionInput;
     private Animator _animator;
+    private bool speed = false;
 
     void Start()
     {
@@ -20,11 +22,25 @@ public class PlayerInput : MonoBehaviour
     void OnInputDuJoueur(InputValue directionBase)
     {
         Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * _mouvement;
-        directionInput = new Vector3(0f, directionAvecVitesse.x, directionAvecVitesse.y);
+        directionInput = new Vector3(0f, directionAvecVitesse.x, directionAvecVitesse.y);       
+        _animator.SetFloat("Haut/Bas", 1f *  directionAvecVitesse.x);
+        _animator.SetFloat("Avant/Arriere", 1f * directionAvecVitesse.y);
     }
+
+    
     void FixedUpdate()
     {
         Vector3 mouvement = directionInput.normalized * _mouvement;
+        
+        if (Keyboard.current.shiftKey.isPressed)
+            {
+                _mouvement = 8;
+            }
+        
+        else{
+            _mouvement = 4;
+        }
         _sub.velocity = mouvement;
+       
     }
 }
